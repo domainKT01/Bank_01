819ca1d31a58174911fcb304a781b59e910133f7
domainKT01 <luisdanielcoronelposada903@gmail.com>
2022-01-27 20:23:35 -0500

    step 1

---
+++ resources/views/components/iniciar_sesion.blade.php
@@ -0,0 +1,50 @@
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+        <x-jet-validation-errors class="mb-4" />
+
+        @if (session('status'))
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ session('status') }}
+        </div>
+        @endif
+
+        <div class="h-screen bg-slate-50 flex justify-center items-center w-full">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <div>
+                    <x-jet-label class="block mb-1 text-gray-600 font-semibold" for="email" value="{{ __('Email') }}" />
+                    <x-jet-input id="email" class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" type="email" name="email" :value="old('email')" required autofocus />
+                </div>
+
+                <div class="mt-4">
+                    <x-jet-label class="block mb-1 text-gray-600 font-semibold" for="password" value="{{ __('Password') }}" />
+                    <x-jet-input id="password" class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" type="password" name="password" required autocomplete="current-password" />
+                </div>
+
+                <div class="block mt-4">
+                    <label for="remember_me" class="flex items-center">
+                        <x-jet-checkbox id="remember_me" name="remember" />
+                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                    @endif
+
+                    <x-jet-button class="ml-4">
+                        {{ __('Log in') }}
+                    </x-jet-button>
+                </div>
+            </form>
+        </div>
+    </x-jet-authentication-card>
+</x-guest-layout>
