11aebd94754ac73c45f9e817b167b8218c7933b6
domainKT01 <luisdanielcoronelposada903@gmail.com>
2022-04-24 17:22:50 -0500

    step 26

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,7 +114,7 @@
                     <div class="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
                         <ul class="list-reset flex flex-row md:flex-col pt-3 md:py-3 px-1 md:px-2 text-center md:text-left">
                             <li class="mr-3 flex-1">
-                                <a href="/BloodBank/public/dashboard" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-blue-600">
+                                <a href="/public/dashboard" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-blue-600">
                                     <i class="h-8 w-8 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                         <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
                                         <circle cx="12" cy="7" r="4">
@@ -122,12 +122,12 @@
                                 </a>
                             </li>
                             <li class="mr-3 flex-1">
-                                <a href="/BloodBank/public/parametros" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500" wire:model="matriz.parametros">
+                                <a href="/public/parametros" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500" wire:model="matriz.parametros">
                                     <i class="fas fa-tasks pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Parametros</span>
                                 </a>
                             </li>
                             <li class="mr-3 flex-1">
-                                <a href="/BloodBank/public/pacientes" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500">
+                                <a href="/public/pacientes" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500">
                                     <i class="fas fa-chart-area pr-0 md:pr-3 text-blue-600"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block">Paciente</span>
                                 </a>
                             </li>
